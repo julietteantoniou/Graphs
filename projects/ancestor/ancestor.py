54ad5de875f6b1da90f9dea292783884f60337f2
Julie Antonio <julieantonio38@gmail.com>
2020-02-18 16:47:56 -0500

    solved ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,17 @@
+from graph import Graph, Stack, Queue
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+    for pair in ancestors:
+        graph.add_edge(pair[1], pair[0])
+    oldest = graph.dft_farthest(starting_node)
+
+    if oldest == None:
+        return -1
+    else:
+        return oldest
